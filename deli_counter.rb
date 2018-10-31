b4ce17177315c29503556f7986494b3598d00e17
Azraf Chowdhury <achowdhur@ltu.edu>
2018-10-31 08:11:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,8 @@ end
 def take_a_number(array, name)
   array.push(name)
   position = array.index(name)
-  
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position 
 end
 
 def now_serving()
