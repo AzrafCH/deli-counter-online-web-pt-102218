e0ad8100ce8abea95647ccd7197a01269be9ed3e
Azraf Chowdhury <achowdhur@ltu.edu>
2018-10-31 08:13:53 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+if array.length == 0 
+puts  "The line is currently empty."
+else
+ say = "The line is currently:"
+ 
+ array.each_with_index do |value, index|
+   say += " #{index.to_i+1}. #{value}"
+ end
+ puts "#{say}"
+ end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position 
+end
+
+def now_serving(array)
+if array.empty? == TRUE
+  puts "There is nobody waiting to be served!"
+  elsif array.empty? == false 
+  puts "Currently serving #{array.shift}."
+end 
+end 
