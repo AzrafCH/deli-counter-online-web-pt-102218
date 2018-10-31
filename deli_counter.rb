f1fb6e9ba503b86b1d77a93d150e0919a91747b6
Azraf Chowdhury <achowdhur@ltu.edu>
2018-10-31 08:03:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,15 +2,7 @@
 katz_deli = []
 
 def line(numinline)
-  line_arr = []
-  if numinline.length == 0
-    puts "The line is currently empty."
-  else
-    line_arr.each.with_index(1) do |name, index|
-      line_arr.push("#{index}: #{name}")
-    end
-    puts "The line is: #{line_arr.join(" ")}"
-  end
+
 end
 
 def take_a_number(katz_deli, name)
